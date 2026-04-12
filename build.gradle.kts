4e3411e04d9d769d386cc9f3ff05d9d2b4652bfc
Linus Törnkrantz <linus@prettybyte.com>
2026-04-12 22:00:22 +0200

    Use klerk custom domain

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,15 +9,15 @@ javaPlatform {
 
 dependencies {
     constraints {
-        api("com.github.klerk-framework:klerk:1.0.0-beta.6")
-        api("com.github.klerk-framework:klerk-web:1.0.0-alpha.2")
-        api("com.github.klerk-framework:klerk-graphql:1.0.0-alpha.2")
+        api("dev.klerkframework:klerk:1.0.0-beta.6")
+        api("dev.klerkframework:klerk-web:1.0.0-alpha.2")
+        api("dev.klerkframework:klerk-graphql:1.0.0-alpha.2")
     }
-    api(platform("io.ktor:ktor-bom:3.4.0"))
+    api(platform("io.ktor:ktor-bom:3.4.2"))
 }
 
 group = "dev.klerkframework"
-version = "1.0.0-beta.6"
+version = "1.0.0-beta.6c"
 
 publishing {
     publications {
