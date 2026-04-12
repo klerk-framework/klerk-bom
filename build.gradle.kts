b27751568831f1a312f08b100d102ea9ff628642
Linus Törnkrantz <linus@prettybyte.com>
2026-04-12 20:05:58 +0200

    Update klerk version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,15 +9,15 @@ javaPlatform {
 
 dependencies {
     constraints {
-        api("dev.klerkframework:klerk:1.0.0-beta.5")
-        api("dev.klerkframework:klerk-web:1.0.0-alpha.3")
-        api("dev.klerkframework:klerk-graphql:1.0.0-alpha.2")
+        api("com.github.klerk-framework:klerk:1.0.0-beta.6")
+        api("com.github.klerk-framework:klerk-web:1.0.0-alpha.2")
+        api("com.github.klerk-framework:klerk-graphql:1.0.0-alpha.2")
     }
     api(platform("io.ktor:ktor-bom:3.4.0"))
 }
 
 group = "dev.klerkframework"
-version = "1.0.0-beta.5"
+version = "1.0.0-beta.6"
 
 publishing {
     publications {
