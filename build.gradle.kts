f4ce10a401541ae3991349741741884f2b128de2
Linus Törnkrantz <linus@prettybyte.com>
2026-04-13 16:06:19 +0200

    snapshot

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,15 +9,15 @@ javaPlatform {
 
 dependencies {
     constraints {
-        api("dev.klerkframework:klerk:1.0.0-beta.6")
-        api("dev.klerkframework:klerk-web:1.0.0-alpha.2")
-        api("dev.klerkframework:klerk-graphql:1.0.0-alpha.2")
+        api("dev.klerkframework:klerk:1.0.0-beta.7-SNAPSHOT")
+        api("dev.klerkframework:klerk-web:1.0.0-alpha.2-SNAPSHOT")
+        api("dev.klerkframework:klerk-graphql:1.0.0-alpha.2-SNAPSHOT")
     }
     api(platform("io.ktor:ktor-bom:3.4.2"))
 }
 
 group = "dev.klerkframework"
-version = "1.0.0-beta.6c"
+version = "1.0.0-beta.7-SNAPSHOT"
 
 publishing {
     publications {
